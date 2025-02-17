5596e468d8a3526625b83bc034b5c0574f5bc4a6
azmenezi <azmenezi@gmail.com>
2025-02-17 18:53:33 +0300

    .

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,11 @@
 fun main() {
-    println("Hello World!")
+    val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val transformedNumbers = numbers.map { it * 2 }
+    println(transformedNumbers)
+
+    val names = listOf("Alice", "Bob", "Amir", "Charlie", "Annie", "David")
+    val filterNames = { listOfName: List<String> -> listOfName.filter { it.contains("a", true) } }
+    println(filterNames(names))
+
+
 }
